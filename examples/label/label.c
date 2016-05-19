9864297392ad66e3d0492bb9e88425fca82c26c2
Leonardo <leonardo.guilherme@gmail.com>
2016-05-19 00:51:39 -0300

    Updates the label example with comments

--- examples/label/label.c
+++ examples/label/label.c
@@ -1,48 +1,64 @@
+/**
+ * This is the `label` example. It displays a label with an icon inside a frame.
+ * The icon is merely a region inside the GUI tileset.
+ */
 #include "KW_gui.h"
 #include "KW_frame.h"
 #include "KW_label.h"
 #include "KW_renderdriver_sdl2.h"
 
 int main(int argc, char ** argv) {
-  /* init SDL and SDL_ttf */
-  KW_RenderDriver * driver;
+  /* We are going to use the SDL2 Render Driver.
+   * Initialize SDL, creating a SDL window and renderer */
+  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Renderer * renderer;
   SDL_Window * window;
-  KW_Surface * set;
-  KW_GUI * gui;
-  KW_Font * font;
-  KW_Widget * frame, * l;
-  KW_Rect geometry, c;
-  
-  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_CreateWindowAndRenderer(320, 240, 0, &window, &renderer);
-  SDL_SetRenderDrawColor(renderer, 100, 100, 100, 1);
-  driver = KW_CreateSDL2RenderDriver(renderer, window);
-  
-  /* load tileset surface */
-  set = KW_LoadSurface(driver, "tileset.png");
-  
-  /* load font */
-  font = KW_LoadFont(driver, "Fontin-Regular.ttf", 12);
-  
-  /* init KiWi */
-  gui = KW_Init(driver, set);
-  KW_SetFont(gui, font);
-  /* create a frame and a label on top of it. */
-  geometry.x = geometry.y = 0; geometry.w = 320; geometry.h = 240;
-  frame = KW_CreateFrame(gui, NULL, &geometry);
-  l = KW_CreateLabel(gui, frame, "Label with an icon :)", &geometry);
-  c.x = 0; c.y = 48; c.w = 24; c.h = 24;
-  KW_SetLabelIcon(l, &c);
+  SDL_SetRenderDrawColor(renderer, 100, 100, 100, 1); /* pretty background */
 
+  /* Now we are going to use the SDL2 Render Driver. Users can implement their own
+   * render driver, as long as it complies to the KW_RenderDriver structure */
+  KW_RenderDriver * driver = KW_CreateSDL2RenderDriver(renderer, window);
+
+  /* Loads the "tileset.png" file as a surface */
+  KW_Surface * set = KW_LoadSurface(driver, "tileset.png");
+
+  /* At this point we can create the GUI. You can have multiple GUI instances
+   * in the same window (or even different windows, its up to the Render Driver) */
+  KW_GUI * gui = KW_Init(driver, set);
+
+  /* Loads a font and sets it. KiWi does not manage foreign memory (it does not own
+   * your pointers), so you must keep them to release later. */
+  KW_Font * font = KW_LoadFont(driver, "Fontin-Regular.ttf", 12);
+  KW_SetFont(gui, font);
+
+  /* Define a geometry and create a frame */
+  KW_Rect geometry = { x: 0, y: 0, w: 320, h: 240 };
+  KW_Widget * frame = KW_CreateFrame(gui, NULL, &geometry);
+
+  /* Now create a label that has the frame as the parent, reutilizing
+   * the same geometry. Children widgets offset they x and y coordinates by their parent. */
+  KW_Widget * label = KW_CreateLabel(gui, frame, "Label with an icon :)", &geometry);
+
+  /* Sets the rect in the tileset to extract the icon from, and sets it in the label */
+  KW_Rect iconrect = { x: 0, y: 48, w : 24, h : 24 };
+  KW_SetLabelIcon(label, &iconrect);
+
+  /* Just call KW_Paint(gui) in your game loop. */
   while (!SDL_QuitRequested()) {
     SDL_RenderClear(renderer);
     KW_Paint(gui);
     SDL_Delay(1);
     SDL_RenderPresent(renderer);
   }
+
+  /* All widgets are free'd after a KW_Quit */
   KW_Quit(gui);
+
+  /* Releases things */
   KW_ReleaseSurface(driver, set);
-  
+  KW_ReleaseFont(driver, font);
+  SDL_Quit();
+
   return 0;
 }
