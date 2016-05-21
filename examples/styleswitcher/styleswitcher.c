800daecd2c95c3eb5c03751098772397a232e6df
Leonardo <leonardo.guilherme@gmail.com>
2016-05-21 00:23:00 -0300

    Port styleswitcher example

--- examples/styleswitcher/styleswitcher.c
+++ examples/styleswitcher/styleswitcher.c
@@ -5,14 +5,16 @@
 #include "KW_editbox.h"
 #include "KW_label.h"
 #include "SDL_image.h"
+#include "KW_renderdriver_sdl2.h"
 
-SDL_Surface * setalloy;
-SDL_Surface * setfutter;
-SDL_Surface * setflat;
-SDL_Surface * set;
+KW_Surface * setalloy;
+KW_Surface * setfutter;
+KW_Surface * setflat;
+KW_Surface * set;
 
-TTF_Font * fontin;
-TTF_Font * sourcepro;
+KW_Font * fontin;
+KW_Font * sourcepro;
+KW_Font * dejavu;
 SDL_bool quit = SDL_FALSE;
 
 void kthxbaiClicked(KW_Widget * widget, int b) {
@@ -52,50 +54,36 @@ void SwitchFutterClicked(KW_Widget * widget, int b) {
 }
 
 int main(int argc, char ** argv) {
+  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window * window;
   SDL_Renderer * renderer;
-  KW_GUI * gui;
-  TTF_Font * dejavu;
-  SDL_Rect framegeom, editgeom, labelgeom, buttongeom;
-  KW_Widget * editbx, * label, * button, * kthxbai, * frame, * buttonsframe;
-  int hinting;
-  
-  /* initialize window and renderer */
-  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_CreateWindowAndRenderer(320, 240, 0, &window, &renderer);
   SDL_SetRenderDrawColor(renderer, 118, 152, 162, 1);
-  TTF_Init();
-  
-  /* load tilesets */
-  setalloy = IMG_Load_RW(SDL_RWFromFile("tileset-alloy.png", "r"), SDL_TRUE);
-  setfutter = IMG_Load_RW(SDL_RWFromFile("tileset-futterpedia.png", "r"), SDL_TRUE);
-  setflat = IMG_Load_RW(SDL_RWFromFile("tileset-flat.png", "r"), SDL_TRUE);
-  set = IMG_Load_RW(SDL_RWFromFile("tileset.png", "r"), SDL_TRUE);
 
-  
-  /* initialize gui */
-  gui = KW_Init(renderer, setfutter);
-  fontin = TTF_OpenFontRW(SDL_RWFromFile("Fontin-Regular.ttf", "r"), SDL_TRUE, 12);
-  sourcepro = TTF_OpenFontRW(SDL_RWFromFile("SourceSansPro-Semibold.ttf", "r"), SDL_TRUE, 12);
-  dejavu = TTF_OpenFontRW(SDL_RWFromFile("DejaVuSans.ttf", "r"), SDL_TRUE, 11);
-  
-  hinting = TTF_HINTING_NONE;
-  TTF_SetFontHinting(fontin, hinting);
-  TTF_SetFontHinting(sourcepro, hinting);
-  TTF_SetFontHinting(dejavu, hinting);
+  KW_RenderDriver * driver = KW_CreateSDL2RenderDriver(renderer, window);
+  setalloy = KW_LoadSurface(driver, "tileset-alloy.png");
+  setfutter = KW_LoadSurface(driver, "tileset-futterpedia.png");
+  setflat = KW_LoadSurface(driver, "tileset-flat.png");
+  set = KW_LoadSurface(driver, "tileset.png");
+
+  KW_GUI * gui = KW_Init(driver, setfutter);
+  fontin = KW_LoadFont(driver, "Fontin-Regular.ttf", 12);
+  sourcepro = KW_LoadFont(driver, "SourceSansPro-Semibold.ttf", 12);
+  dejavu = KW_LoadFont(driver, "DejaVuSans.ttf", 11);
   KW_SetFont(gui, fontin);
+
+  KW_Rect framegeom;
 
   /* top frame */  
   framegeom.x = 10, framegeom.y = 10, framegeom.w = 300, framegeom.h = 220;
-  frame = KW_CreateFrame(gui, NULL, &framegeom);
+  KW_Widget * frame = KW_CreateFrame(gui, NULL, &framegeom);
   
   /* buttons frame */
   framegeom.x = 10, framegeom.y = 160, framegeom.w = 280, framegeom.h = 48;
-  buttonsframe = KW_CreateFrame(gui, frame, &framegeom);
-  
-  /* buttons */
-  button = NULL;
-  buttongeom.x = 10, buttongeom.y = 8, buttongeom.w = 32, buttongeom.h = 32;
+  KW_Widget * buttonsframe = KW_CreateFrame(gui, frame, &framegeom);
+
+  KW_Rect buttongeom = { x: 10, y: 8, w: 32, h: 32 };
+  KW_Widget * button;
   button = KW_CreateButton(gui, buttonsframe, "", &buttongeom);
   KW_SetWidgetTilesetSurface(button, set);
   KW_AddWidgetMouseDownHandler(button, SwitchNormalClicked);
@@ -120,21 +108,22 @@ int main(int argc, char ** argv) {
   framegeom.w -= 20; framegeom.h = 100;
   
   /* create the editbox frames */
-  editgeom.x = 120, editgeom.y = 20, editgeom.w = 150, editgeom.h = 35;
-  labelgeom.x = 10, labelgeom.y = 20, labelgeom.w = 110, labelgeom.h = 35;
+  KW_Rect editgeom = { x: 120, y: 20, w: 150, h: 35 };
+  KW_Rect labelgeom = { x: 10, y: 20, w: 110, h: 35 };
   frame = KW_CreateFrame(gui, frame, &framegeom);
-  editbx = KW_CreateEditbox(gui, frame, "βέβαιος (sure)", &editgeom);
+  KW_Widget * editbx = KW_CreateEditbox(gui, frame, "βέβαιος (sure)", &editgeom);
   KW_SetEditboxFont(editbx, dejavu);
-  label = KW_CreateLabel(gui, frame, "Can you do UTF-8?", &labelgeom);
+  KW_Widget * label = KW_CreateLabel(gui, frame, "Can you do UTF-8?", &labelgeom);
   KW_SetLabelAlignment(label, KW_LABEL_ALIGN_RIGHT, 0, KW_LABEL_ALIGN_MIDDLE, 0);
   
   buttongeom = editgeom;
   buttongeom.y = 60; buttongeom.h -= 10;
-  kthxbai = KW_CreateButton(gui, frame, "kthxbai", &buttongeom);
+  KW_Widget * kthxbai = KW_CreateButton(gui, frame, "kthxbai", &buttongeom);
   KW_AddWidgetMouseDownHandler(kthxbai, kthxbaiClicked);
   
   while (!SDL_QuitRequested() && !quit) {
     SDL_RenderClear(renderer);
+    KW_ProcessEvents(gui);
     KW_Paint(gui);
     SDL_RenderPresent(renderer);
     SDL_Delay(1);
@@ -142,13 +131,13 @@ int main(int argc, char ** argv) {
   
   /* free stuff */
   KW_Quit(gui);
-  TTF_CloseFont(fontin);
-  TTF_CloseFont(dejavu);
-  SDL_FreeSurface(set);
-  SDL_FreeSurface(setalloy);
-  SDL_FreeSurface(setflat);
-  SDL_FreeSurface(setfutter);
-  TTF_Quit();
+
+  KW_ReleaseFont(driver, fontin);
+  KW_ReleaseFont(driver, dejavu);
+  KW_ReleaseSurface(driver, set);
+  KW_ReleaseSurface(driver, setalloy);
+  KW_ReleaseSurface(driver, setflat);
+  KW_ReleaseSurface(driver, setfutter);
   SDL_Quit();
   
   return 0;
