a8977d0153e1f7e72f7498a4211f0468df428ef2
Leonardo <leonardo.guilherme@gmail.com>
2016-05-21 01:03:23 -0300

    Documenting sdl2 render driver

--- src/KW_renderdriver_sdl2.h
+++ src/KW_renderdriver_sdl2.h
@@ -6,6 +6,13 @@ struct SDL_Window;
 
 #include "KW_macros.h"
 
+/**
+ * \brief   Creates a SDL2 based KiWi render driver
+ * \details First you need to create the SDL2 Renderer and the Window, and then pass them to this function
+ * \param   renderer The SDL2 Renderer
+ * \param   window The SDL2 window
+ * \returns A KW_RenderDriver instance
+ */
 extern DECLSPEC struct KW_RenderDriver * KW_CreateSDL2RenderDriver(struct SDL_Renderer * renderer, struct SDL_Window * window);
 
 #endif
