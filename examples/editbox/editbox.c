fc9158122e17de1745416dd38b9f1e4a699977d6
Leonardo <leonardo.guilherme@gmail.com>
2016-05-21 23:11:42 -0300

    Refactor the edibotx example

--- examples/editbox/editbox.c
+++ examples/editbox/editbox.c
@@ -1,60 +1,57 @@
-#include "SDL.h"
+/**
+ * This is the `editbox` example. It displays an editbox, labels and buttons.
+ */
+
 #include "KW_gui.h"
-#include "KW_button.h"
 #include "KW_frame.h"
 #include "KW_editbox.h"
 #include "KW_label.h"
+#include "KW_button.h"
 #include "KW_renderdriver_sdl2.h"
 
+/* Callback for when the OK button is clicked */
+KW_bool quit = KW_FALSE;
+void OKClicked(KW_Widget * widget, int b) {
+  quit = KW_TRUE;
+}
 
 int main(int argc, char ** argv) {
+  /* Initialize SDL */
+  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window * window;
   SDL_Renderer * renderer;
-  KW_RenderDriver * driver;
-  KW_Surface * set;
-  KW_GUI * gui;
-  KW_Rect framegeom, editgeom, labelgeom, buttongeom;
-  KW_Widget * frame, * editbx, * label;
-  KW_Font * fontin, * dejavu;
-  
-  /* initialize window and renderer, and create a render driver */
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_CreateWindowAndRenderer(320, 240, 0, &window, &renderer);
+  KW_Rect windowrect = { 0, 0, 320, 240 };
+  SDL_CreateWindowAndRenderer(windowrect.w, windowrect.h, 0, &window, &renderer);
   SDL_SetRenderDrawColor(renderer, 100, 100, 200, 1);
-  driver = KW_CreateSDL2RenderDriver(renderer, window);
 
-  /* load tileset */
-  set = KW_LoadSurface(driver, "tileset.png");
-  
-  /* initialize gui */
-  gui = KW_Init(driver, set);
-  fontin = KW_LoadFont(driver, "Fontin-Regular.ttf", 12);
-  dejavu = KW_LoadFont(driver, "DejaVuSans.ttf", 12);
-  KW_SetFont(gui, fontin);
+  /* Initialize KiWi */
+  KW_RenderDriver * driver = KW_CreateSDL2RenderDriver(renderer, window);
+  KW_Surface * set = KW_LoadSurface(driver, "tileset.png");
+  KW_GUI * gui = KW_Init(driver, set);
+  KW_Font * dejavu = KW_LoadFont(driver, "DejaVuSans.ttf", 12);
+  KW_SetFont(gui, dejavu);
 
-  framegeom.x = 10, framegeom.y = 10, framegeom.w = 300, framegeom.h = 220;
-  frame = KW_CreateFrame(gui, NULL, &framegeom);
-  buttongeom.x = 120, buttongeom.y = 110, buttongeom.w = 170, buttongeom.h = 30;
-  KW_CreateButton(gui, frame, "Friendship? Again?!", &buttongeom);
-  
-  framegeom.w -= 20; framegeom.h = 100;
-  editgeom.x = 120, editgeom.y = 20, editgeom.w = 150, editgeom.h = 30;
-  labelgeom.x = 10, labelgeom.y = 20, labelgeom.w = 110, labelgeom.h = 30;
-  frame = KW_CreateFrame(gui, frame, &framegeom);
-  editbx = KW_CreateEditbox(gui, frame, "Editbox #1", &editgeom);
-  KW_SetEditboxFont(editbx, dejavu);
-  label = KW_CreateLabel(gui, frame, "Type your destiny:", &labelgeom);
-  KW_SetLabelAlignment(label, KW_LABEL_ALIGN_RIGHT, 0, KW_LABEL_ALIGN_MIDDLE, 0);
-  
-  editgeom.x = 120, editgeom.y = 50, editgeom.w = 150, editgeom.h = 30;
-  labelgeom.x = 10, labelgeom.y = 50, labelgeom.w = 110, labelgeom.h = 30;
-  editbx = KW_CreateEditbox(gui, frame, "Editbox #2", &editgeom);
-  KW_SetEditboxFont(editbx, dejavu);
-  
-  label = KW_CreateLabel(gui, frame, "Again:", &labelgeom);
-  KW_SetLabelAlignment(label, KW_LABEL_ALIGN_RIGHT, 0, KW_LABEL_ALIGN_MIDDLE, 0);
+  /* Create the top-level framve */
+  KW_Rect framerect = { x: 10, y: 10, w: 300, h: 220 };
+  KW_RectCenterInParent(&windowrect, &framerect);
+  KW_Widget * frame = KW_CreateFrame(gui, NULL, &framerect);
 
-  while (!SDL_QuitRequested()) {
+  /* Create the title, label and edibox widgets */
+  KW_Rect titlerect = { x: 0, y: 10, w: 300, h: 30 };
+  KW_Rect labelrect = { y: 100, w: 60, h: 30 };
+  KW_Rect editboxrect = { y: 100, w: 100, h: 30 };
+  KW_Rect * rects[] = { &labelrect, &editboxrect };
+  unsigned weights[] = { 1, 4 };
+  KW_RectFillParentHorizontally(&framerect, rects, weights, 2, 10, KW_RECT_ALIGN_MIDDLE);
+  KW_CreateLabel(gui, frame, "Editbox example", &titlerect);
+  KW_CreateLabel(gui, frame, "Label", &labelrect);
+  KW_CreateEditbox(gui, frame, "Edit me!", &editboxrect);
+  KW_Rect buttonrect = { x: 250, y: 170, w: 40, h: 40 };
+  KW_Widget * okbutton = KW_CreateButton(gui, frame, "OK", &buttonrect);
+  KW_AddWidgetMouseDownHandler(okbutton, OKClicked);
+
+  /* Main loop */
+  while (!SDL_QuitRequested() && !quit) {
     SDL_RenderClear(renderer);
     KW_ProcessEvents(gui);
     KW_Paint(gui);
@@ -64,7 +61,6 @@ int main(int argc, char ** argv) {
   
   /* free stuff */
   KW_Quit(gui);
-  KW_ReleaseFont(driver, fontin);
   KW_ReleaseFont(driver, dejavu);
   KW_ReleaseSurface(driver, set);
   KW_ReleaseRenderDriver(driver);
