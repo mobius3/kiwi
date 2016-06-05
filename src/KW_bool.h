5a7efae47dc9842b833a27d49c62911e3a3ba04c
Leonardo <leonardo.guilherme@gmail.com>
2016-06-05 01:08:29 -0300

    Adding KW_bool.h, remove it from KW_widget.h

---
+++ src/KW_bool.h
@@ -0,0 +1,15 @@
+#ifndef KW_BOOL_H
+#define KW_BOOL_H
+
+/**
+ * \file KW_bool.h
+ *
+ * A helper enumeration with true and false values
+ */
+
+typedef enum {
+  KW_FALSE = 0,
+  KW_TRUE = 1
+} KW_bool;
+
+#endif
