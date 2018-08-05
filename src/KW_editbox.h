1d596f468bc602bffcc86da49016a19d21da165a
Leonardo <leonardo.guilherme@gmail.com>
2018-08-05 16:28:41 -0300

    add documentation for kw_editbox.h (#32)

--- src/KW_editbox.h
+++ src/KW_editbox.h
@@ -38,16 +38,61 @@
 extern "C" {
 #endif
 
+/**
+ * \brief   Creates an editbox widget.
+ * \details The editbox uses tiles in the third column of the tileset
+ * \param   gui The KW_GUI instance that will hold this widget.
+ * \param   parent The parent widget of this widget.
+ * \param   text The initial text for this editbox
+ * \param   geometry The relative geometry of this frame.
+ * \return  The editbox instance.
+ */
 extern DECLSPEC KW_Widget * KW_CreateEditbox(KW_GUI * gui, KW_Widget * parent,
                                              const char * text,
                                              const KW_Rect * geometry);
 
+/**
+ * \brief Specifies the editbox text
+ * \details Cursor is reset
+ * \param widget the editbox widget to change
+ * \param text the text to set
+ */
 extern DECLSPEC void KW_SetEditboxText(KW_Widget * widget, const char * text);
+
+/**
+ * \brief Returns the text in the editbox
+ * \param widget The editbox instance to return text from
+ * \return the text in the editbox
+ */
 extern DECLSPEC const char * KW_GetEditboxText(KW_Widget * widget);
+
+/**
+ * \brief Specify, in terms of characters, the position of the cursor
+ * \param widget the edtibox widget to change
+ * \param pos the cursor position
+ */
 extern DECLSPEC void KW_SetEditboxCursorPosition(KW_Widget * widget,
                                                  unsigned int pos);
+
+/**
+ * \brief Returns, in terms of characters, where de cursor is
+ * \param widget the editbox widget to return the cursor from
+ * \return the cursor position
+ */
 extern DECLSPEC unsigned int KW_GetEditboxCursorPosition(KW_Widget * widget);
+
+/**
+ * \brief Changes the current font used to render edtibox text.
+ * \param widget The editbox widget to change font
+ * \param font the KW_Font instance to use
+ */
 extern DECLSPEC void KW_SetEditboxFont(KW_Widget * widget, KW_Font * font);
+
+/**
+ * \brief Returns the current KW_Font being used in the editbox
+ * \param widget the editbox widget to get the font from
+ * \return the font being used
+ */
 extern DECLSPEC KW_Font * KW_GetEditboxFont(KW_Widget * widget);
 
 /**
