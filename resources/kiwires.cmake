c7eeaf598d3caf7589f6f90fdf7e49bd0469d292
Peter <info@emersont1.co.uk>
2019-06-12 18:16:54 +0000

    various fixes to allow it to be used as a subdirectory This creates a KIWI_ROOT variable that replaces CMAKE_SOURCE_DIR in a few places. This allows it to be built via the add_subdirectory() function

--- resources/kiwires.cmake
+++ resources/kiwires.cmake
@@ -5,7 +5,7 @@ macro(kiwires)
     cmake_parse_arguments(KIWIRES "" "${ONEVALUEARGS}" "${MULTIVALUEARGS}" ${ARGN})
     try_compile(FILE2C_RESULT ${CMAKE_BINARY_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/file2c.c COPY_FILE "${CMAKE_BINARY_DIR}/file2c${CMAKE_EXECUTABLE_SUFFIX}")
     execute_process(
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            WORKING_DIRECTORY ${KIWI_ROOT}
             COMMAND
                 ${CMAKE_BINARY_DIR}/file2c${CMAKE_EXECUTABLE_SUFFIX}
                     ${CMAKE_CURRENT_BINARY_DIR}/${KIWIRES_HEADER}
