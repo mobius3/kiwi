319c9dabc4f037407d63c63133036a8e44a3efb9
Leonardo <leonardo.guilherme@gmail.com>
2019-11-03 01:39:07 -0300

    add core.h and widgets.h

---
+++ include/core.h
@@ -0,0 +1,14 @@
+#ifndef KW_CORE_H
+#define KW_CORE_H
+
+#include "core/bool.h"
+#include "core/eventwatcher.h"
+#include "core/gui.h"
+#include "core/macros.h"
+#include "core/renderdriver.h"
+#include "core/renderdriver_sdl2.h" // TODO: uncouple
+#include "core/tilerenderer.h"
+#include "core/utf8.h"
+#include "core/widget.h"
+
+#endif
---
+++ include/widgets.h
@@ -0,0 +1,12 @@
+#ifndef KW_WIDGETS_H
+#define KW_WIDGETS_H
+
+#include "widgets/button.h"
+#include "widgets/checkbox.h"
+#include "widgets/editbox.h"
+#include "widgets/frame.h"
+#include "widgets/label.h"
+#include "widgets/scrollbox.h"
+#include "widgets/toggle.h"
+
+#endif
