99ce9d9ea329582e0cfe8f12fb7ba7d0578287eb
Peter Taylor <info@emersont1.co.uk>
2020-05-12 07:39:20 +0100

    fixed example sizing

--- examples/checkbox-radio/checkbox-radio.c
+++ examples/checkbox-radio/checkbox-radio.c
@@ -11,47 +11,35 @@
 #include "KW_button.h"
 #include "KW_renderdriver_sdl2.h"
 
-KW_Widget * editBox;
-
-/* Callback for when the OK button is clicked */
-KW_bool quit = KW_FALSE;
-void OKClicked(KW_Widget * widget, int b) {
-  const char* text = KW_GetEditboxText(editBox);
-  printf("%s \n", text);
-  quit = KW_TRUE;
-}
-
 int main(int argc, char ** argv) {
   /* Initialize SDL */
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window * window;
   SDL_Renderer * renderer;
-  KW_Rect windowrect = { 0, 0, 500, 500 };
+  KW_Rect windowrect = { 0, 0, 320, 240 };
   SDL_CreateWindowAndRenderer(windowrect.w, windowrect.h, 0, &window, &renderer);
-  SDL_SetRenderDrawColor(renderer, 100, 100, 200, 1);
+  SDL_SetRenderDrawColor(renderer, 0x47, 0x97, 0xde, 1);
 
   /* Initialize KiWi */
   KW_RenderDriver * driver = KW_CreateSDL2RenderDriver(renderer, window);
   KW_Surface * set = KW_LoadSurface(driver, "tileset.png");
   KW_GUI * gui = KW_Init(driver, set);
 
-  KW_Rect r = {x : 25, y : 25, w : 450, h : 50};
+  KW_Rect r = {x : 25, y : 25, w : 270, h : 32};
   KW_CreateLabel(gui, NULL, "Checkbox/Radiobox Demo", &r);
-  r.y+=50;
-
+  r.y+=32;
     r.w = 100;
     KW_Widget  *checkbox;
     KW_Rect checked = { .x = 96, .y = 24, .w = 24, .h = 24 };
     KW_Rect unchecked = { .x = 96, .y = 0, .w = 24, .h = 24 };
-    for(int i =0;i<8;i++){
-      if(i%3==0 && i >0){r.x+=150;r.y-=150;}
-      r.y+=50;
-      checkbox = KW_CreateCheckbox(gui, NULL, "Checkbox", &r);
+      checkbox = KW_CreateCheckbox(gui, NULL, "Checkbox A", &r);
       KW_CheckboxSetChecked(checkbox, &checked);
       KW_CheckboxSetUnchecked(checkbox, &unchecked);
-    }
-
-    r.x-=300;
+    r.x+=175;
+    checkbox = KW_CreateCheckbox(gui, NULL, "Checkbox B", &r);
+    KW_CheckboxSetChecked(checkbox, &checked);
+    KW_CheckboxSetUnchecked(checkbox, &unchecked);
+    r.x=25;
     r.y+=50;
 
     KW_Rect rchecked = { .x = 72, .y = 0, .w = 24, .h = 24 };
@@ -60,14 +48,15 @@ int main(int argc, char ** argv) {
     KW_RadioManagerSetUnchecked(m, &unchecked);
 
     for(int i =0;i<4;i++){
-      r.y+=50;
+      if(i%2==0 && i >0){r.x+=175;r.y-=2*r.h;}
       checkbox = KW_CreateRadio(gui, NULL, m, "Radio", i+1, &r);
+      r.y+=r.h;
     }
 
 
 
   /* Main loop */
-  while (!SDL_QuitRequested() && !quit) {
+  while (!SDL_QuitRequested()) {
     SDL_RenderClear(renderer);
     KW_ProcessEvents(gui);
     KW_Paint(gui);
